71448a92b1c5bee79019297fdc9eac5f7a37db6d
Vladyslav Yanov <2vladooyanov6@gmail.com>
2023-06-04 11:34:24 +0300

    First version

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,10 +1,18 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ContentTypes
+from aiogram.dispatcher.filters import Text
+
+from tgbot.keyboards.reply import kbd
 
 
 async def user_start(message: Message):
     await message.reply("Hello, user!")
 
 
+async def test_message(message: Message):
+    await message.answer("Test message", reply_markup=kbd)
+
+
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_message_handler(test_message, Text(equals=["Test", "Program"]))
---
+++ tgbot/keyboards/reply.py
@@ -0,0 +1,3 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+kbd = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[[KeyboardButton(text="🏆")]])
